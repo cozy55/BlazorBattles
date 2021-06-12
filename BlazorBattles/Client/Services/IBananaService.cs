ee5cc8bba1c9944b195c7cddb231d7379d5a8941
cozy55 <skeletbrown@gmail.com>
2021-06-12 19:32:26 +0300

    Army and built pages were added

---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    interface IBananaService
+    {
+        event Action OnChange;
+
+        int Bananas{ get; set; }
+
+        void EatBananas(int amount);
+
+        void AddBananas(int amount);
+    }
+}
